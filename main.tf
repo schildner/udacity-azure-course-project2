a246463ce0800eacff94ad32e320f526da934a4b
schildner <eduard.schildner@gmail.com>
2020-12-28 04:03:15 +0100

    Add terraform files.

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+  tags = {
+    project_name = "CI/CD"
+    stage        = "Submission2"
+  }
+}
+
+resource "azurerm_app_service_plan" "main" {
+  name                = "${var.prefix}-asp"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+
+  tags = {
+    project_name = "CI/CD"
+    stage        = "Submission2"
+  }
+}
+
+resource "azurerm_app_service" "main" {
+  name                = "${var.prefix}-appservice"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+
+  tags = {
+    project_name = "CI/CD"
+    stage        = "Submission2"
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "udacity-azure-course-project2-cicd"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "westeurope"
+}
