86431ae647ef8b48b2bce98320f6e9e3d95bc4e3
schildner <eduard.schildner@gmail.com>
2020-12-28 20:23:39 +0100

    Revert changes from hello.py.

--- hello.py
+++ hello.py
@@ -10,23 +10,3 @@ def add(x):
 
 def subtract(x):
     return x - 1
-
-
-def json_payload(): 
-    payload = { 
-        "CHAS": {"0": 0}, 
-        "RM": {"0": 6.575},
-        "TAX": {"0": 296.0}, 
-        "PTRATIO": {"0": 15.3}, 
-        "B": {"0": 396.9}, 
-        "LSTAT": {"0": 4.98} 
-    }
-    
-    jsonData = json.dumps(payload)
-    print(jsonData)
-
-json_payload()
-
-print("Status code: ", response.status_code)
-print("Printing Entire Post Request")
-print(response.json())
