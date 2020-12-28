75df40cfde31da868913dd4f36d37288b06329b9
schildner <eduard.schildner@gmail.com>
2020-12-28 04:21:51 +0100

    Script to deploy webapp.

---
+++ commands.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Update: Resource group now created by terraform! Uncomment if necessary.
+# Create Resource Group in nearest Location necessary for running a webapp
+# az group create --name udacity-azure-course-project1-iac-rg --location westeurope
+
+# Deploy webapp 
+az webapp up -n udacity-azure-course-project2-cicd-appservice \
+      --sku F1 \
+      --resource-group udacity-azure-course-project2-cicd-rg \
+      -l westeurope
