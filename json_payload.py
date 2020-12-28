6cec22f1ad226a3d1ec7f4360d6e79bb440e6d16
schildner <eduard.schildner@gmail.com>
2020-12-28 16:38:45 +0100

    Mock file for testing http post request and response.

---
+++ json_payload.py
@@ -0,0 +1,26 @@
+
+import requests
+import json
+
+def json_payload(): 
+    payload = { 
+        "CHAS": {"0": 0}, 
+        "RM": {"0": 6.575},
+        "TAX": {"0": 296.0}, 
+        "PTRATIO": {"0": 15.3}, 
+        "B": {"0": 396.9}, 
+        "LSTAT": {"0": 4.98} 
+    }
+    
+    jsonData = json.dumps(payload)
+    print(jsonData)
+
+    response = requests.post('https://udacity-azure-course-project2-cicd-appservice.azurewebsites.net:443', 
+        json=payload)
+
+    print("Status code: ", response.status_code)
+    print("Printing Entire Post Request")
+    print(response.json())
+
+json_payload()
+
