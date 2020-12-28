9a44d65edcd3c68130103ee1e19c1a9d27b1c9f4
schildner <eduard.schildner@gmail.com>
2020-12-28 16:26:28 +0100

    Add json payload to python dummy file.

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+import json
+
 def toyou(x):
     return "hi %s" % x
 
@@ -8,3 +10,23 @@ def add(x):
 
 def subtract(x):
     return x - 1
+
+
+def json_payload(): 
+    payload = { 
+        "CHAS": {"0": 0}, 
+        "RM": {"0": 6.575},
+        "TAX": {"0": 296.0}, 
+        "PTRATIO": {"0": 15.3}, 
+        "B": {"0": 396.9}, 
+        "LSTAT": {"0": 4.98} 
+    }
+    
+    jsonData = json.dumps(payload)
+    print(jsonData)
+
+json_payload()
+
+print("Status code: ", response.status_code)
+print("Printing Entire Post Request")
+print(response.json())
