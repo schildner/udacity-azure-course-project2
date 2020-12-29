36cc1c2821eda5989dc931e39899bccd33693249
schildner <eduard.schildner@gmail.com>
2020-12-29 22:24:00 +0100

    Added wait_time to locustfile + wait.

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,18 @@
 import json
 import time
 import random
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
+    wait_time = between(3, 5)
+
     host = "https://udacity-azure-course-project2-cicd-appservice.azurewebsites.net:443"
 
-    @task
+    @task(1)
     def hello_world(self):
         self.client.get("https://udacity-azure-course-project2-cicd-appservice.azurewebsites.net")
   
-    @task
+    @task(5)
     def predict(self):
 
         """Input data explanation (from https://www.kaggle.com/c/boston-housing) 
@@ -21,15 +23,13 @@ class MyUser(HttpUser):
         b:          1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town.
         lstat:      lower status of the population (percent).
         """
-
-        for item in range(10):
-            payload={ 
-                "CHAS": {"0": random.randint(0,1)}, 
-                "RM": {"0": random.uniform(1.5, 10.0)},
-                "TAX": {"0": random.uniform(99.0, 999.0)}, 
-                "PTRATIO": {"0": random.uniform(5.0, 45.0)}, 
-                "B": {"0": random.uniform(10.0, 999.0)}, 
-                "LSTAT": {"0": random.uniform(1.5, 9.9)} 
-            }
-            response = self.client.post("/predict", json=payload, headers={'Content-Type': 'application/json'})
-            time.sleep(1)
+        payload={ 
+            "CHAS": {"0": random.randint(0,1)}, 
+            "RM": {"0": random.uniform(1.5, 10.0)},
+            "TAX": {"0": random.uniform(99.0, 999.0)}, 
+            "PTRATIO": {"0": random.uniform(5.0, 45.0)}, 
+            "B": {"0": random.uniform(10.0, 999.0)}, 
+            "LSTAT": {"0": random.uniform(1.5, 9.9)} 
+        }
+        response = self.client.post("/predict", json=payload, headers={'Content-Type': 'application/json'})
+        time.sleep(1)
