a0b1e1252a9dabb958c4c9c6ff9f0a8149fe48c4
Eduard Schildner <eduard.schildner@gmail.com>
2020-12-30 18:22:04 +0100

    Version 2.0

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home, app version 2.0</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
